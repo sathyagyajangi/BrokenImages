dce67e398fbc2af1c11eff2c7e2104bb6790f9d8
Sathyanarayana Gyajangi <Sathyanarayana@sportzinteractive.com>
2019-12-10 18:05:17 +0530

    latest commit

--- FindingBrokenImages/FunctionLibrary.cs
+++ FindingBrokenImages/FunctionLibrary.cs
@@ -88,7 +88,7 @@ namespace FindingBrokenImages
         {
             excel.Application xlapp = new excel.Application();
 
-            excel.Workbook xlworkbook = xlapp.Workbooks.Open(@"C:\Users\Satyanarayan\source\git\BrokenImages\FindingBrokenImages\TestInput\TestInput.xlsx");
+            excel.Workbook xlworkbook = xlapp.Workbooks.Open(@"C:\Users\Satyanarayan\source\git\BrokenImages\FindingBrokenImages\TestInput\input.xlsx");
 
             excel._Worksheet xlworksheet = xlworkbook.Sheets[S];
 
